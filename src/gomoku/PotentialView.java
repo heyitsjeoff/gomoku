a2ce419b56f76e278b94d83f6caad15a7f42f014
heyitsjeoff <heyitsjeoff@gmail.com>
2015-04-29 02:24:08 -0700

    Doorstep

--- src/gomoku/PotentialView.java
+++ src/gomoku/PotentialView.java
@@ -11,7 +11,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 
