010537298e1c95c1c95513dcedba6d45053122e0
heyitsjeoff <heyitsjeoff@gmail.com>
2015-05-01 17:33:47 -0700

    created cell class

--- src/gomoku/GameModel.java
+++ src/gomoku/GameModel.java
@@ -1,12 +1,3 @@
-/*
-Jeoff Villanueva
-Project
-CSCE 320 Spring
-Date
-Java used in Netbeans
-Sources:
-
- */
 package gomoku;
 
 
--- src/gomoku/PotentialView.java
+++ src/gomoku/PotentialView.java
@@ -15,43 +15,41 @@ import javax.swing.JTextArea;
 
 
 public class PotentialView extends JFrame{
-    
+    private JPanel panelCenter = new JPanel();
+    private int row, col;
+    private MyJButton[][] square;
+    private JTextArea displayArea;
+    private int count;
+    private final int MAX_COUNT=4;
+    private JButton step;
 
-	private JPanel panelCenter = new JPanel();
-	private int row, col;
-	private MyJButton[][] square;
-	private JTextArea displayArea;
-	private int count;
-	private final int MAX_COUNT=4;
-	private JButton step;
-
-	public PotentialView(){
-		row = 30;
-		col = 30;
+    public PotentialView(){
+	row = 30;
+	col = 30;
 		
-		this.setLayout(new BorderLayout());
+	this.setLayout(new BorderLayout());
 		
-		//set up north layout
-		step = new JButton("Send Move");
-		step.addActionListener(new ButtonListener());
-		this.add(step,BorderLayout.NORTH);
+	//set up north layout
+	step = new JButton("Send Move");
+	step.addActionListener(new ButtonListener());
+	this.add(step,BorderLayout.NORTH);
 		
-		//set up grid in for the center panel
-		panelCenter.setLayout(new GridLayout(row,col));
-		square = new MyJButton[row][col];
-		SquareListener listener = new SquareListener();
-		for(int i =0; i<row; i++) {
-			for (int j=0; j<col; j++){
-				square[i][j]= new MyJButton(); 
-				square[i][j].i=i;
-				square[i][j].j=j;
-				//square[i][j].setSize(20,20);
-				square[i][j].setBackground(Color.white);
-				square[i][j].addActionListener(listener);
-				panelCenter.add(square[i][j]);
-			}
-		}
-	    this.add(panelCenter,BorderLayout.CENTER);
+	//set up grid in for the center panel
+	panelCenter.setLayout(new GridLayout(row,col));
+	square = new MyJButton[row][col];
+	SquareListener listener = new SquareListener();
+	for(int i =0; i<row; i++) {
+            for (int j=0; j<col; j++){
+		square[i][j]= new MyJButton(); 
+		square[i][j].i=i;
+		square[i][j].j=j;
+		//square[i][j].setSize(20,20);
+		square[i][j].setBackground(Color.white);
+		square[i][j].addActionListener(listener);
+		panelCenter.add(square[i][j]);
+            }
+	}
+	this.add(panelCenter,BorderLayout.CENTER);
 		
 		//set up text area in the south
 		displayArea = new JTextArea( 8, 30 ); // set up JTextArea
@@ -66,24 +64,24 @@ public class PotentialView extends JFrame{
 		//this.pack();
 		
 	}
+        
+    public void updateGrid(){
+        count = 0;
+    }
 	
-	private class SquareListener implements ActionListener{
-
-		public void actionPerformed(ActionEvent e) {
-			
-                    MyJButton button = (MyJButton) e.getSource();
-			//System.out.println("this is button:"+button.i + " " +button.j);
-			if(count<1 && button.getBackground().equals(Color.white)) {
-					button.setBackground(Color.green);
-                                        count++;
-			}
-                        else if(button.getBackground().equals(Color.green)){
-                            button.setBackground(Color.white);
-                            count--;
-                        }			 		
-		}
-		
-	}
+    private class SquareListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            MyJButton button = (MyJButton) e.getSource();
+            if(count<1 && button.getBackground().equals(Color.white)) {
+		button.setBackground(Color.green);
+                count++;
+            }
+            else if(button.getBackground().equals(Color.green)){
+                button.setBackground(Color.white);
+                count--;
+            }			 		
+        }
+    }
 	
 	private class ButtonListener implements ActionListener{
 
